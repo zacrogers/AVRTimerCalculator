4a447f309f82b39f65e65bcbd5cec836c0a59fa6
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-22 17:27:49 +1300

    Update avr_timer_calc.py

--- avr_timer_calc.py
+++ avr_timer_calc.py
@@ -1,9 +1,9 @@
 import math
 
+clk_freq = 8000000
 
-clk_freq = 0
-prescaler = 0
-timer_resolution = 0
+prescaler = 1 #1, 8, 32, 64, 128, 256, 1024
+timer_resolution = 8 #8, 16, 32
 
 total_ticks = 0
 overflows = 0
@@ -20,14 +20,39 @@ def calc_total():
   real_time = total_ticks / freq
   new_freq = freq / total_ticks
 
+  print_state()
 
 def calc_overflow_remainder():
-  pass
+  freq = clk_freq / prescaler
+  
+  total_ticks = overflows * (2**timer_resolution)
+  print_state()
     
     
 def calc_real_time():
-  pass
+  new_freq = 1 / real_time
   
+  freq = clk_freq / prescaler
+  total_ticks = real_time * freq
+  
+  overflow = math.floor(total_ticks / (2**timer_resolution))
+  remainder = total_ticks - (overflow * (2**timer_resolution))
+  
+  print_state()
   
 def calc_new_freq():
-  pass
+  real_time = 1 / new_freq
+  freq = clk_freq / prescaler
+  
+  total_ticks = real_time * freq
+  
+  overflow = math.floor(total_ticks / (2**timer_resolution))
+  remainder = total_ticks - (overflow * (2**timer_resolution))
+  
+  print_state()
+
+  
+  
+def print_state():
+  print("System clock:{} \n Timer Res:{}\n Prsc:{}\n Tot ticks:{}\n".format(clk_freq, prescaler, timer_resolution, total_ticks))
+  print("Overflow:{} \n Remainder:{}\n Real time:{}\n New icks:{}\n".format(overflows, remainder, real_time, new_freq))
